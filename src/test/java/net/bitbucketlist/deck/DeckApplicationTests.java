21a481fd4c3e431ec586e66e9c54091559ce5569
Brian O'Mara <bomara512@gmail.com>
2020-05-26 19:55:46 -0500

    Introduce Spring Boot

---
+++ src/test/java/net/bitbucketlist/deck/DeckApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bitbucketlist.deck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
