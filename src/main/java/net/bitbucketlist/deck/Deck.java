1be73736c6378267a63c8114ea4e867ae0aceae3
bomara <bomara512@gmail.com>
2020-05-28 14:59:52 -0500

    Add deck

---
+++ src/main/java/net/bitbucketlist/deck/Deck.java
@@ -0,0 +1,40 @@
+package net.bitbucketlist.deck;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    List<Card> deck = new ArrayList<>(52);
+
+    public Deck() {
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                deck.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public List<Card> deal(int numberOfCards) {
+        if (size() < numberOfCards) {
+            throw new IllegalArgumentException(
+                String.format("Can't deal %s cards, only %s card(s) left in deck", numberOfCards, size())
+            );
+        }
+
+        List<Card> dealtCards = new ArrayList<>();
+        for (int i = 0; i < numberOfCards; i++) {
+            Card dealtCard = deck.remove(0);
+            dealtCards.add(dealtCard);
+        }
+        return dealtCards;
+    }
+
+    public int size() {
+        return deck.size();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+}
---
+++ src/test/java/net/bitbucketlist/deck/DeckTest.java
@@ -0,0 +1,64 @@
+package net.bitbucketlist.deck;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DeckTest {
+    private static List<Card> UNSHUFFLED_DECK;
+
+    private final Deck subject = new Deck();
+
+    @BeforeAll
+    static void beforeAll() {
+        UNSHUFFLED_DECK = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                UNSHUFFLED_DECK.add(new Card(suit, rank));
+            }
+        }
+
+    }
+
+    @Test
+    void newDeck_has52Cards() {
+        assertThat(subject.size()).isEqualTo(52);
+    }
+
+    @Test
+    void newDeck_isUnshuffled() {
+        assertThat(subject.deal(52)).isEqualTo(UNSHUFFLED_DECK);
+    }
+
+    @Test
+    void deal() {
+        List<Card> actual = subject.deal(5);
+
+        assertThat(actual).isEqualTo(UNSHUFFLED_DECK.subList(0, 5));
+
+        assertThat(subject.size()).isEqualTo(47);
+    }
+
+    @Test
+    void deal_notEnoughCardsLeft_throwsException() {
+        subject.deal(51);
+
+        assertThatThrownBy(() -> subject.deal(2))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Can't deal 2 cards, only 1 card(s) left in deck");
+    }
+
+    @Test
+    void shuffle() {
+        subject.shuffle();
+
+        List<Card> actual = subject.deal(52);
+
+        assertThat(actual).isNotEqualTo(UNSHUFFLED_DECK);
+    }
+}
